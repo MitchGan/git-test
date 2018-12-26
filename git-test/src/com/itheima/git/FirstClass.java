0fd0c10364d6d3e4c941f48f23ae4db5533a4e44
mitch <821993913@qq.com>
2018-12-27 01:00:17 +0800

    提交代码

---
+++ git-test/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("git真好用");
+        System.out.println("git真好用");
+        System.out.println("git真好用");
+        System.out.println("git真好用");
+        System.out.println("git真好用");
+        System.out.println("git真好用");
+    }
+}
